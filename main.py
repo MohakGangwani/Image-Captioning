5a531433d12448fa72aa5d24e85dfe6be416b267
Mohak Gangwani <mohakmg99@gmail.com>
2024-10-18 14:38:34 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.applications import InceptionV3
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.inception_v3 import preprocess_input
